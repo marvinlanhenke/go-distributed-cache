5e3765395a31f3449db78890d79564370867879e
marvinlanhenke <lanhenke@googlemail.com>
2024-10-15 10:28:23 +0200

    feat: add RWMutex to cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,10 +1,13 @@
 package cache
 
+import "sync"
+
 type CacheItem struct {
 	Value string
 }
 
 type Cache struct {
+	mu    sync.RWMutex
 	items map[string]CacheItem
 }
 
@@ -15,10 +18,14 @@ func New() *Cache {
 }
 
 func (c *Cache) Set(key, value string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.items[key] = CacheItem{Value: value}
 }
 
 func (c *Cache) Get(key string) (string, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	item, ok := c.items[key]
 	if !ok {
 		return "", false
--- internal/cache/cache_test.go
+++ internal/cache/cache_test.go
@@ -1,6 +1,8 @@
 package cache_test
 
 import (
+	"strconv"
+	"sync"
 	"testing"
 
 	"github.com/marvinlanhenke/go-distributed-cache/internal/cache"
@@ -15,4 +17,33 @@ func TestCacheSetGet(t *testing.T) {
 
 	require.True(t, ok, "failed to get a value")
 	require.Equal(t, value, "value_a", "failed to get the correct value")
+}
+
+func TestCacheConcurrency(t *testing.T) {
+	cache := cache.New()
+	var wg sync.WaitGroup
+
+	numGoroutines := 10
+	opsPerGoroutine := 1000
+
+	for i := 0; i < numGoroutines; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			for j := 0; j < opsPerGoroutine; j++ {
+				key := strconv.Itoa(j)
+				value := "value" + key
+
+				cache.Set(key, value)
+				result, ok := cache.Get(key)
+
+				if ok {
+					require.Equal(t, value, result, "unexpected value for key %s: got %s, expected %s", key, result, value)
+				}
+
+			}
+		}()
+	}
 }
