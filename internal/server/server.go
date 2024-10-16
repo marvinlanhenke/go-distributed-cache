aa3a8aaffaba1cd538eceff3162e103f9cdc2082
marvinlanhenke <lanhenke@googlemail.com>
2024-10-16 16:40:55 +0200

    feat: impl hashring on server

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,29 +7,61 @@ import (
 	empty "github.com/golang/protobuf/ptypes/empty"
 	"github.com/marvinlanhenke/go-distributed-cache/internal/cache"
 	"github.com/marvinlanhenke/go-distributed-cache/internal/config"
+	"github.com/marvinlanhenke/go-distributed-cache/internal/hashring"
 	"github.com/marvinlanhenke/go-distributed-cache/internal/pb"
 	"golang.org/x/time/rate"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type cacheServer struct {
 	pb.UnimplementedCacheServiceServer
-	cache   *cache.Cache
-	config  *config.Config
-	limiter *rate.Limiter
+	cache    *cache.Cache
+	hashRing *hashring.HashRing
+	config   *config.Config
+	limiter  *rate.Limiter
 }
 
 func New(cfg *config.Config) *cacheServer {
 	cs := &cacheServer{
-		cache:   cache.New(cfg.Capacity, cfg.TTL*time.Second),
-		config:  cfg,
-		limiter: rate.NewLimiter(rate.Limit(cfg.RateLimit), cfg.RateLimitBurst),
+		cache:    cache.New(cfg.Capacity, cfg.TTL*time.Second),
+		hashRing: hashring.New(),
+		config:   cfg,
+		limiter:  rate.NewLimiter(rate.Limit(cfg.RateLimit), cfg.RateLimitBurst),
 	}
+
+	for _, peer := range cfg.Peers {
+		if peer != "" {
+			cs.hashRing.Add(&hashring.Node{ID: peer, Addr: peer})
+		}
+	}
+	cs.hashRing.Add(&hashring.Node{ID: cfg.Addr, Addr: cfg.Addr})
 
 	return cs
 }
 
 func (cs *cacheServer) Set(ctx context.Context, req *pb.SetRequest) (*empty.Empty, error) {
-	cs.cache.Set(req)
+	if req.Key == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid request: you must provide a valid key")
+	}
+
+	isReplication := req.SourceNode != ""
+	if isReplication {
+		cs.cache.Set(req)
+		return &empty.Empty{}, nil
+	}
+
+	targetNode, ok := cs.hashRing.Get(req.Key)
+	if !ok {
+		return &empty.Empty{}, nil
+	}
+
+	if targetNode.Addr == cs.config.Addr {
+		cs.cache.Set(req)
+		go cs.replicate(req)
+		return &empty.Empty{}, nil
+	}
+	// TODO: forward
 	return &empty.Empty{}, nil
 }
 
@@ -39,4 +71,8 @@ func (cs *cacheServer) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResp
 		return nil, nil
 	}
 	return resp, nil
+}
+
+func (cs *cacheServer) replicate(req *pb.SetRequest) {
+	// TODO: impl client pool
 }
