2c5a5bda7ce588ab659398bb850f2b2ed45fccfc
marvinlanhenke <lanhenke@googlemail.com>
2024-10-19 05:53:53 +0200

    chore: add docs

--- internal/cache/shard.go
+++ internal/cache/shard.go
@@ -6,13 +6,18 @@ import (
 	"time"
 )
 
+// Represents a partition of the cache that stores a subset of cache items.
 type shard struct {
-	mu       sync.RWMutex
-	items    map[string]*list.Element
-	eviction *list.List
-	capacity int
+	mu       sync.RWMutex             // Mutex for synchronizing read and write access to the shard.
+	items    map[string]*list.Element // Map for fast lookup of cache items by key.
+	eviction *list.List               // Doubly linked list to track item usage for LRU eviction.
+	capacity int                      // Maximum number of items the shard can hold before eviction is triggered.
 }
 
+// Checks if a cache item has expired based on its TTL (time-to-live).
+//
+// If the item is expired, it is removed from both the eviction list and the items map.
+// Returns true if the item was evicted, false otherwise.
 func (s *shard) evictTTL(item *cacheItem, elem *list.Element, key string) bool {
 	if time.Now().After(item.expiryTime) {
 		s.eviction.Remove(elem)
@@ -22,6 +27,8 @@ func (s *shard) evictTTL(item *cacheItem, elem *list.Element, key string) bool {
 	return false
 }
 
+// Evicts the least-recently-used (LRU) item from the shard when the capacity is exceeded.
+// The item at the back of the eviction list (the least recently used) is removed from both the list and the items map.
 func (s *shard) evictLRU() {
 	elem := s.eviction.Back()
 	if elem != nil {
