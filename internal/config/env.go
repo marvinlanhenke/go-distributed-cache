e570b75f9135de08d9015cba8e331e94016519be
marvinlanhenke <lanhenke@googlemail.com>
2024-10-20 05:47:23 +0200

    chore: add docs

--- internal/config/env.go
+++ internal/config/env.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 )
 
+// Retrieves the value of the environment variable identified by 'key'.
+// If the environment variable is not set, it returns the provided 'fallback' value.
 func getString(key, fallback string) string {
 	val, ok := os.LookupEnv(key)
 	if !ok {
@@ -14,6 +16,8 @@ func getString(key, fallback string) string {
 	return val
 }
 
+// Retrieves the value of the environment variable identified by 'key' and converts it to an integer.
+// If the environment variable is not set or the value cannot be parsed as an integer, it returns the provided 'fallback' value.
 func getInt(key string, fallback int) int {
 	val, ok := os.LookupEnv(key)
 	if !ok {
