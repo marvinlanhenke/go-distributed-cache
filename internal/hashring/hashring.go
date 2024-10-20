1fb25a6fc5d36029630c77e741fd78e5c9c2ff80
marvinlanhenke <lanhenke@googlemail.com>
2024-10-20 05:50:52 +0200

    chore: add docs

--- internal/hashring/hashring.go
+++ internal/hashring/hashring.go
@@ -7,34 +7,43 @@ import (
 	"sync"
 )
 
+// Represents a node in the hash ring, identified by its unique ID and associated with an address.
 type Node struct {
-	ID   string
-	Addr string
+	ID   string // Unique identifier of the node.
+	Addr string // Network address of the node.
 }
 
+// Represents a node in the hash ring, along with its hashed key value.
 type member struct {
-	hash uint32
-	node *Node
+	hash uint32 // Hash of the node's ID.
+	node *Node  // Pointer to the actual node.
 }
 
+// HashRing represents a consistent hash ring used for distributing keys across nodes.
+// It supports adding, removing, and retrieving nodes based on the hash of a key.
 type HashRing struct {
-	mu          sync.Mutex
-	members     []member
-	Replication int
+	mu          sync.Mutex // Mutex to ensure thread-safe operations on the ring.
+	members     []member   // Slice of members (nodes) in the hash ring.
+	Replication int        // Number of nodes to replicate each key to.
 }
 
+// Creates and returns an empty HashRing instance.
 func New() *HashRing {
 	return &HashRing{}
 }
 
+// Returns the number of members currently in the hash ring.
 func (hr *HashRing) Size() int {
 	return len(hr.members)
 }
 
+// Checks if the hash ring has no members and returns true if empty, false otherwise.
 func (hr *HashRing) IsEmpty() bool {
 	return hr.Size() == 0
 }
 
+// Adds a new node to the hash ring, hashing the node's ID and inserting it into the sorted list of members.
+// The replication factor is updated after the node is added.
 func (hr *HashRing) Add(node *Node) {
 	hr.mu.Lock()
 	defer hr.mu.Unlock()
@@ -50,6 +59,7 @@ func (hr *HashRing) Add(node *Node) {
 	hr.Replication = hr.Size()/2 + 1
 }
 
+// Removes a node from the hash ring by its ID, adjusting the list of members accordingly.
 func (hr *HashRing) Remove(nodeID string) {
 	hr.mu.Lock()
 	defer hr.mu.Unlock()
@@ -62,6 +72,8 @@ func (hr *HashRing) Remove(nodeID string) {
 	}
 }
 
+// Returns a list of nodes that should be responsible for the given key based on its hash.
+// The number of nodes returned is determined by the replication factor. If enough nodes cannot be found, it returns false.
 func (hr *HashRing) GetNodes(key string) ([]*Node, bool) {
 	if hr.IsEmpty() || hr.Replication > hr.Size() {
 		return nil, false
@@ -107,6 +119,8 @@ func (hr *HashRing) GetNodes(key string) ([]*Node, bool) {
 	return nodes, true
 }
 
+// Computes a 32-bit hash of a given key using the SHA-1 hashing algorithm.
+// The first 4 bytes of the SHA-1 hash are used to generate the 32-bit hash value.
 func (hr *HashRing) hash(key string) uint32 {
 	sum := sha1.Sum([]byte(key))
 	return binary.LittleEndian.Uint32(sum[:4])
