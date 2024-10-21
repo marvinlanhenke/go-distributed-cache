2bad7906a58ed18efb2c35dba8c8c6fdaf8717a3
marvinlanhenke <lanhenke@googlemail.com>
2024-10-21 07:06:13 +0200

    chore: add docs

--- internal/server/member.go
+++ internal/server/member.go
@@ -7,22 +7,34 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+// Implements the memberlist.EventDelegate interface, providing handlers for membership events
 type eventDelegate struct {
-	*cacheServer
+	*cacheServer // Embedded cacheServer instance to interact with the cache and hash ring.
 }
 
+// NotifyJoin is called when a new node joins the memberlist cluster.
+// It logs the event and adds the node to the hash ring.
 func (d *eventDelegate) NotifyJoin(node *memberlist.Node) {
 	log.Info().Str("node", node.Name).Msg("Node joined")
 	d.hashRing.Add(&hashring.Node{ID: node.Name, Addr: node.Name})
 }
 
+// NotifyLeave is called when a node leaves the memberlist cluster.
+// It logs the event and removes the node from the hash ring.
 func (d *eventDelegate) NotifyLeave(node *memberlist.Node) {
 	log.Info().Str("node", node.Name).Msg("Node left")
 	d.hashRing.Remove(node.Name)
 }
 
+// NotifyUpdate is called when a node in the memberlist cluster is updated.
+// This implementation does nothing, but it can be extended to handle node updates.
 func (d *eventDelegate) NotifyUpdate(node *memberlist.Node) {}
 
+// Creates and configures a new memberlist for managing the cluster's membership,
+// using the provided cache server and configuration settings.
+//
+// It sets up the memberlist with the eventDelegate to handle node join and leave events,
+// and attempts to join the cluster using the peers specified in the configuration.
 func newMemberlist(cs *cacheServer, cfg *config.Config) *memberlist.Memberlist {
 	mlConfig := memberlist.DefaultLANConfig()
 	mlConfig.Name = cfg.Addr
@@ -30,11 +42,11 @@ func newMemberlist(cs *cacheServer, cfg *config.Config) *memberlist.Memberlist {
 
 	ml, err := memberlist.Create(mlConfig)
 	if err != nil {
-		log.Fatal().Err(err).Msg("failed to create memberlist")
+		log.Fatal().Err(err).Msg("Failed to create memberlist")
 	}
 
 	if _, err := ml.Join(cfg.Peers); err != nil {
-		log.Warn().Err(err).Msg("failed to join membership cluster")
+		log.Warn().Err(err).Msg("Failed to join membership cluster")
 	}
 
 	return ml
